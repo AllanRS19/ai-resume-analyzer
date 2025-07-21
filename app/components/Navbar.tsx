4aa0d9319e68c3e5d5beb1300b8c0f25c3f0efeb
Allan <allanrs19@gmail.com>
2025-07-21 18:41:33 -0400

    Cloud storage, PDF-Image conversion, Key-Value storage and AI feedback

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router";
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <Link to="">
+            <Link to="/">
                 <p className="text-2xl font-bold text-gradient">RESUMIND</p>
             </Link>
             <Link to="upload" className="primary-button w-fit">
